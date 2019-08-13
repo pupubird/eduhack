a241a958e5ad2c584207d6e550b18795c1f9f034
Rain Chai <rainchai4240@gmail.com>
2019-08-14 00:09:47 +0800

    form

--- javascript/main.js
+++ javascript/main.js
@@ -64,6 +64,71 @@ plusPar.addEventListener("click", () => {
 
     //participants details
     var div = document.createElement("DIV");
+
+    div.innerHTML = `
+    <div>
+        <label for="par${form_ammount.length - 1}_name">Full Name</label>
+        <input type="text" name="par${form_ammount.length - 1}_name">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_ic">IC/Passport Number</label>
+        <input type="text" name="par${form_ammount.length - 1}_ic">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_gender">Gender</label>
+        <select name="par${form_ammount.length - 1}_gender">
+            <option selected>Select</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="pref_not">Prefer not to say</option>
+        </select>
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_mobile">Mobile phone</label>
+        <input type="text" name="par${form_ammount.length - 1}_mobilephone">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_email">Email</label>
+        <input type="email" name="par${form_ammount.length - 1}_email">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_university">University</label>
+        <input type="text" name="par${form_ammount.length - 1}_university">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_programme">Programme</label>
+        <input type="text" name="par${form_ammount.length - 1}_programme">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_tshirt">T-shirt size</label>
+        <select name="par${form_ammount.length - 1}_tshirt">
+            <option selected>Select</option>
+            <option value="s">S</option>
+            <option value="m">M</option>
+            <option value="l">L</option>
+            <option value="xl">XL</option>
+        </select>
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_vege">Vegetarian</label>
+        <input type="checkbox" name="par${form_ammount.length - 1}_vege" id="vege">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_emer_contact">Emergency contact
+            number</label>
+        <input type="text" name="par${form_ammount.length - 1}_emer_contact">
+    </div>
+    <div>
+        <label for="par${form_ammount.length - 1}_emer_contact_name">Emergency contact
+            name</label>
+        <input type="text" name="par${form_ammount.length - 1}_emer_contact_name">
+    </div>
+    
+    `;
+    console.log(form_ammount.length - 1);
+
+
+
     div.setAttribute("class", "panel");
     formAccor.insertBefore(div, plusPar);
 
